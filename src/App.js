22cc436c1926563327597ed80da3ee19571b3963
Ivan Vargas <xenevreu@gmail.com>
2022-03-16 22:20:50 -0300

    pushing code

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React V2 with amplify
         </a>
       </header>
     </div>
