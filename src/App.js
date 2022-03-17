d44a0714be5c088f3d446f05678de26d869155cf
Ivan Vargas <xenevreu@gmail.com>
2022-03-17 00:04:41 -0300

    push

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       {({ signOut, user }) => (
         <main>
           <h1>Hello {user.attributes.email}</h1>
-          <button onClick={signOut}>Sign out v2</button>
+          <button onClick={signOut}>Sign out</button>
         </main>
       )}
     </Authenticator>
